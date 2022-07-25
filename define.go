b69bde57ad125c9977c7a723d223b6483fde8086
hwcer <hwclegend@163.com>
2022-07-25 16:49:39 +0800

    dev

--- define.go
+++ define.go
@@ -26,11 +26,11 @@ var (
 )
 
 type Cache struct {
-	OID   string      `json:"_id"`
-	IID   int32       `json:"id"`
-	Key   string      `json:"k"`
-	Val   interface{} `json:"v"`
-	Bag   int32       `json:"b"`
+	OID string      `json:"_id"`
+	IID int32       `json:"id"`
+	Key string      `json:"k"`
+	Val interface{} `json:"v"`
+	//Bag   int32       `json:"b"`
 	Ret   interface{} `json:"r"`
 	AType ActType     `json:"t"`
 	IType IType       `json:"-"`
