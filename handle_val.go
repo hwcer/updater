6393e7ad580b1b144cc800eb2e7a96a445abbd07
万超 黄 <hwclegend@163.com>
2024-10-17 15:36:46 +0800

    update

--- handle_val.go
+++ handle_val.go
@@ -74,6 +74,8 @@ func (this *Values) reset() {
 		if this.Updater.Error = this.save(); this.Updater.Error != nil {
 			logger.Alert("保存数据失败,name:%v,data:%v\n%v", this.name, this.dataset, this.Updater.Error)
 		} else {
+			this.dataset = dataset.NewValues()
+			this.statement.loader = false
 			this.Updater.Error = this.loading(this.statement.ram)
 		}
 	}
